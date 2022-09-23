0f1a4c52febba17fd98cda01aae581006efc4083
Alina <ars224@lehigh.edu>
2022-09-23 13:51:26 -0400

    Assignment 2R

---
+++ Assignment 2.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(here)
+us_states <- read.csv(here("covid-19-data","us-states.csv"))
+us_states
+library(dplyr)
+Pennsylvania <- filter(us_states , state == "Pennsylvania")
+Incr_cases <- us_states %>%
+  mutate(cases = cases)
+Incr_deaths <- us_states %>% 
+    mutate(deaths = deaths)
+sd(us_states$Incr_cases)
