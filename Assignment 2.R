cace5db45369208eff9ced1b64588a3bdab555f1
Rishika <rig220@lehigh.edu>
2022-09-26 10:14:12 -0400

    Grading

--- Assignment 2.R
+++ Assignment 2.R
@@ -5,11 +5,15 @@ us_states <- read.csv(here("covid-19-data","us-states.csv"))
 us_states
 library(dplyr)
 Pennsylvania <- filter(us_states , state == "Pennsylvania")
+
+#   Failure to remember to set incremental cases and deaths to levels on row 1: -10 
+#   Failure to implement for() loop successfully: -10 
+
 Incr_cases <- us_states %>%
   mutate(cases = cases)
 Incr_deaths <- us_states %>% 
     mutate(deaths = deaths)
 sd(us_states$Incr_cases)
 =======
-#Missing RScript
+
 >>>>>>> main
